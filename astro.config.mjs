f5c3841400b1f7effa95b9aae7896b64c5fc814b
DeanKuhn <dakuhn1138@gmail.com>
2026-06-08 13:22:41 -0500

    Changed base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://deankuhn.dev',
-  // For GitHub Pages, uncomment and set your repo name:
-  // base: '/your-repo-name',
+  base: '/deanslist',
 });
